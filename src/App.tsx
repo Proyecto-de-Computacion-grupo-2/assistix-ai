33fc4e8c146aa19900d057ca859d32bd276134ef
LCabelloC <lcabelloms@gmail.com>
2024-03-01 13:49:36 +0100

    Comienzo assistix-ai

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import LoginPage from './pages/login-page/login-page'
+
+function App() {
+
+  return (
+    <>
+      <LoginPage />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,23 @@
+import './login-page.scss'
+
+import assistixLogo from './assets/AssistixAI_logo.png'
+
+export default function LoginPage() {
+
+    return (
+        <div id='login-container'>
+            <div className="login-container-blur">
+                <form className="login-container">
+                    <img src={assistixLogo} alt="Assistix Logo" />
+                    <h1>Assistix AI</h1>
+                    <div className="login-form">
+                        <input type="text" placeholder="Ingrese su correo" />
+                        <input type="password" placeholder="Ingrese su contraseña" />
+                        <button>Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+
+}
