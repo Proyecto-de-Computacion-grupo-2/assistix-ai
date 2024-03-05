48a27160ee068787b4748aebf81cf790d2316d83
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-05 19:40:21 +0100

    Make the header with SCSS

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,42 +1,43 @@
 import home_icon from '../../assets/home.svg';
 import star_icon from '../../assets/star.svg';
 import user_logo from '../../assets/user-logo.png';
+import './header.scss'
 
 
 export default function Header() {
     return (
         <header>
-            <div>
+            <div className="fc">
                 <div>
-                    <div>
+                    <div className="fc">
                         <img src={home_icon} alt='Home icon'/>
-                        <p>Home</p>
+                        <p>&gt; Home</p>
                     </div>
                     <div>
                         <p>Próxima jornada:17:30:59</p>
                     </div>
+                </div>
+                <div className="fc">
                     <div>
-                        <div>
-                            <p>Saldo Actual</p>
-                            <p>team.saldo_actual</p>
-                        </div>
-                        <div>
-                            <p>Saldo futuro</p>
-                            <p>team.saldo_futuro</p>
-                        </div>
-                        <div>
-                            <p>Deuda máxima</p>
-                            <p>team.deuda_maxima</p>
-                        </div>
+                        <p>Saldo Actual</p>
+                        <p>team.saldo_actual</p>
                     </div>
                     <div>
+                        <p>Saldo futuro</p>
+                        <p>team.saldo_futuro</p>
+                    </div>
+                    <div>
+                        <p>Deuda máxima</p>
+                        <p>team.deuda_maxima</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="fc" >
                         <div>
-                            <div>
-                                <p>team.name</p>
-                                <img src={star_icon} alt='Star icon'/>
-                            </div>
-                            <img src={user_logo} alt='User logo'/>
+                            <p>team.name</p>
+                            <img src={star_icon} alt='Star icon'/>
                         </div>
+                        <img src={user_logo} alt='User logo'/>
                     </div>
                 </div>
             </div>
