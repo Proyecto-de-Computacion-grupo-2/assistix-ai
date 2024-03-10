99b9ece4ec782f8af58c87db8f32592fcc569f03
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-10 16:57:14 +0100

    Added children call to the DP

--- src/pages/dashboard/dashboard-page.tsx
+++ src/pages/dashboard/dashboard-page.tsx
@@ -1,16 +1,9 @@
 import Layout from '../../components/layout/layout.tsx'
-import './dashboard-page.scss'
 
 export default function DashboardPage() {
 
     return (
-        <>
-            <Layout>
-                <div>
-                    <h1>Probando que funcion el layout</h1>
-                </div>
-            </Layout>
-        </>
+        <Layout children={null}/>
     )
 
 }
