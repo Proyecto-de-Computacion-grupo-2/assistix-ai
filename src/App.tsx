1361edec24de54be98018eba64887aa6bf0eab04
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-10 18:18:11 +0100

    Calling Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Dashboard from './pages/dashboard/dashboard-page.tsx';
+//import Dashboard from './pages/dashboard/dashboard-page.tsx';
+import Settings from './pages/settings/settings.tsx';
+
 import './index.scss'
 
 export default function App() {
 
     return (
         <>
-          <Dashboard/>
+          <Settings/>
         </>
     )
 }
