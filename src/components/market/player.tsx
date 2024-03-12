38da14dc0f445f593646ccc05b0f45cf97183e78
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-12 21:44:53 +0100

    Added the player info

---
+++ src/components/market/player.tsx
@@ -0,0 +1,57 @@
+import player_face from '../../assets/player-face-example.png';
+import './player.scss';
+
+function PlayerData() {
+    return (
+        <div className='d-flex flex-column align-items-center ms-3 me-3'>
+            <p className='mb-1' style={{fontWeight:'bold'}}>DL</p>
+            <p style={{fontWeight:'bold'}}>118</p>
+        </div>
+    )
+}
+
+function PlayerImage() {
+    return (
+        <div>
+            <img src={player_face}/>
+        </div>
+    )
+}
+
+function PlayerGameWeeks() {
+    return (
+        <div className='d-flex flex-column mt-2 ms-3'>
+            <p style={{fontWeight:'bold'}}>Y. En Nesyri</p>
+            <div className='d-flex'>
+                <PlayerGameWeek/>
+                <PlayerGameWeek/>
+                <PlayerGameWeek/>
+                <PlayerGameWeek/>
+            </div>
+        </div>
+    )
+}
+
+function PlayerGameWeek() {
+    return (
+        <div className='d-flex flex-column align-items-center me-2'>
+            <p className='game-week-points'>9</p>
+            <p className='game-week-number' style={{ color: '#4EB3A4' }}>J22</p>
+        </div>
+    )
+}
+
+export default function PlayerMarket() {
+    return (
+        <>
+            <div className='mt-3 mb-3 border-top border-bottom'>
+                <p className='mt-3 ms-3'>Libre, termina en PROPS</p>
+                <div className='d-flex align-items-center'>
+                    <PlayerData/>
+                    <PlayerImage/>
+                    <PlayerGameWeeks/>
+                </div>
+            </div>
+        </>
+    )
+}
