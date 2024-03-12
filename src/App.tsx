069c65ee2f7436640e8abbc779e687ab3f16401f
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-12 21:46:34 +0100

    Add market and player pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 //import Dashboard from './pages/dashboard/dashboard-page.tsx';
-import Settings from './pages/settings/settings.tsx';
-import Assistant from './pages/assistant/assistant.tsx';
+//import Settings from './pages/settings/settings.tsx';
+//import Assistant from './pages/assistant/assistant.tsx';
+import Market from './pages/market/market.tsx';
 
-import './index.scss'
 
 export default function App() {
 
     return (
         <>
-          <Assistant/>
+          <Market/>
         </>
     )
 }
---
+++ src/components/market/PlayerInMarket.tsx
@@ -0,0 +1,15 @@
+import PlayerMarket from '../../components/market/player.tsx'
+
+export default function PlayerInMarket() {
+
+    return (
+        <>
+            <div>
+                <p>Mercado</p>
+                <div>
+                    <PlayerMarket/>
+                </div>
+            </div>
+        </>
+    )
+}
