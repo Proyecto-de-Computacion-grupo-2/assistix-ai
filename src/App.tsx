a697be30c98ed76d34a9271a13883b38b0f11d14
LCabelloC <lcabelloms@gmail.com>
2024-03-13 18:49:15 +0100

    Arreglo layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import Assistant from './pages/assistant/assistant.tsx';
 import Market from './pages/market/market.tsx';
 import Player from './pages/player/player.tsx';
 import Settings from './pages/settings/settings.tsx';
-import {Routes, Route} from "react-router-dom";
-
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Dashboard/>}/>
-                <Route path="/assistant" element={<Assistant/>}/>
-                <Route path="/market" element={<Market/>}/>
-                <Route path="/players" element={<Player/>}/>
-                <Route path="/settings" element={<Settings/>}/>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/assistant" element={<Assistant />} />
+                <Route path="/market" element={<Market />} />
+                <Route path="/players" element={<Player />} />
+                <Route path="/settings" element={<Settings />} />
             </Routes>
         </>
     )
--- src/pages/dashboard/dashboard-page.tsx
+++ src/pages/dashboard/dashboard-page.tsx
@@ -1,4 +1,4 @@
-import { Container, } from 'react-bootstrap'
+import { Col, Container, Row, } from 'react-bootstrap'
 import Layout from '../../components/layout/layout.tsx'
 import './dashboard-page.scss'
 import LineUp from '../../components/line-up/line-up.tsx'
@@ -12,7 +12,32 @@ export default function DashboardPage() {
     return (
         <>
             <Layout>
-                <Container className='d-flex flex-grow-1 flex-row align-items-center p-0' fluid>
+                <Row className='p-0 m-0 h-100 gx-3' fluid>
+                    <Col className='p-0 me-2'>
+                        <Row className='p-0 m-0'>
+                            <Col sm={6} className='p-0 m-0'>
+                                <LigaRanking />
+                            </Col>
+                            <Col sm={6} className='p-0 m-0'>
+                                <RecomendationSection />
+                            </Col>
+                        </Row>
+                        {/* <Row>
+                            <Col className='p-0 m-0'>
+                                <PricesComponent />
+                            </Col>
+                            <Col className='p-0 m-0'>
+                                <PujadosSection />
+                            </Col>
+                        </Row>
+                        <Row>
+                        </Row> */}
+                    </Col>
+                    <Col className='p-0 m-0 bg-white rounded-4 ms-2'>
+
+                    </Col>
+                </Row>
+                {/* <Container className='d-flex flex-grow-1 flex-row align-items-center p-0' fluid>
                     <div className='container my-container rounded p-0' style={{ width: '49%', height: '98%' }}>
                         <div className="container d-flex justify-content-between flex-column p-0" style={{ height: '100%' }}>
                             <div className="row" style={{ height: '33%' }}>
@@ -38,7 +63,7 @@ export default function DashboardPage() {
                         </div>
                     </div>
                     <LineUp />
-                </Container >
+                </Container > */}
             </Layout >
         </>
     )
