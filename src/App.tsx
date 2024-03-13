ac015ad2a64b1bc34da898d14c43c615e80343fb
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-13 13:08:12 +0100

    final changes luis update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-//import Dashboard from './pages/dashboard/dashboard-page.tsx';
+import Dashboard from './pages/dashboard/dashboard-page.tsx';
 //import Settings from './pages/settings/settings.tsx';
 //import Assistant from './pages/assistant/assistant.tsx';
-import Market from './pages/market/market.tsx';
+//import Market from './pages/market/market.tsx';
 
 
 export default function App() {
