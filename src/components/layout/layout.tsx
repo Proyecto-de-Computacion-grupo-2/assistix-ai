8ae26b10088870abda7e2679e5d6c644091dc939
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-13 22:52:32 +0100

    Deleted legacy code

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -62,36 +62,3 @@ export default function Layout({ children }: { children: any }) {
         </Container>
     );
 }
-
-/*
-const teamInfo = {
-    name: "UA2C",
-    next_gameweek: "17:30:59",
-    current_balance: "17560860",
-    future_balance: "17560860",
-    maximum_debt: "42311110",
-    points: "748",
-};
-
-export default function Layout({ children }: { children: any }) {
-    return (
-        <Container className="vh-100 m-0 p-2 bg-dark" fluid>
-            <Row className="h-100 p-0 m-0 gx-2" fluid>
-                <Col sm={2} className="d-flex flex-column justify-content-between">
-                    <SideBarNavMoney />
-                </Col>
-                <Col sm={10}>
-                    <Container className="h-100 m-0 p-0 rounded-4 d-flex flex-column gap-2" fluid>
-                        <Row className="p-0 m-0">
-                            <Header teamInfo={teamInfo} />
-                        </Row>
-                        <Row className="p-0 m-0 flex-grow-1">
-                            {children}
-                        </Row>
-                    </Container>
-                </Col>
-            </Row>
-        </Container>
-    )
-}
- */
