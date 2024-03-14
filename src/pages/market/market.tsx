b22b363b18eba5adfebe62995bc58005c042e4c4
LCabelloC <lcabelloms@gmail.com>
2024-03-14 14:49:32 +0100

    Añado carga data desde json y ajuste automatico de la variacion del precio y precio actual

--- src/pages/market/market.tsx
+++ src/pages/market/market.tsx
@@ -7,22 +7,57 @@ import './market.scss'
 import Ramanzani from '../../assets/40090.png'
 import { ChartComponent } from '../../components/market-components/chart/chart.tsx';
 import RecomendationSection from '../../components/dashboard-components/recomendation-section/recomendation.tsx';
+import { useEffect, useState } from 'react';
+
+import json from './prueba.json';
+
 interface ChartData {
     time: string;
     value: number;
 }
 
-const initialData: ChartData[] = [
-    { time: '2023-03-14', value: 184000 },
-    { time: '2023-03-15', value: 160000 },
-    { time: '2023-03-16', value: 201000 },
-    { time: '2023-03-17', value: 200000 },
-    { time: '2023-03-18', value: 166000 },
-    { time: '2023-03-19', value: 173000 },
-    { time: '2023-03-20', value: 171000 },
-];
+interface InputFormat {
+    value: number;
+    date: string;
+}
+
+function convertFormat(input: InputFormat[]): ChartData[] {
+    return input.map(item => {
+        const date = new Date(Date.parse(item.date.replace(/(\d+) (\w+)/, "$2 $1, ")));
+        date.setHours(12); // Ajusta la hora a mediodía
+        return {
+            time: date.toISOString().split('T')[0],
+            value: item.value
+        };
+    });
+}
 
 export default function Market() {
+
+    const [data, setData] = useState<ChartData[]>([]);
+    const [marketPrice, setMarketPrice] = useState('0');
+    const [variation, setVariation] = useState('0%');
+
+    //TODO: Fetch data from API
+    // async function fetchData() {
+    //     const response = await fetch(`${json}`);
+    //     const data = await response.json();
+
+    //     return data.points;
+    // }
+
+
+    function processData() {
+        const data = convertFormat(json.points as InputFormat[]);
+        setData(data);
+        setMarketPrice(`${(data[data.length - 1].value).toLocaleString('es-ES')} €`);
+        setVariation(`${((data[data.length - 1].value - data[data.length - 2].value) / data[data.length - 2].value * 100).toFixed(2)}%`);
+    }
+
+    useEffect(() => {
+        processData();
+    }, []);
+
 
     return (
         <Layout>
@@ -40,7 +75,7 @@ export default function Market() {
                                         <Container className='d-flex flex-column p-0 m-0'>
                                             <p className='fs-5 bold text-secondary'>PRECIO ACTUAL</p>
                                             <Container className='d-flex flex-row p-0 m-0 gap-1'>
-                                                <p className='fs-3 bold '>12.391.000</p>
+                                                <p className='fs-3 bold '>{marketPrice}</p>
                                             </Container>
                                         </Container>
                                     </Container>
@@ -48,11 +83,11 @@ export default function Market() {
                                         <div className="player-container rounded-4 bg-dark my-2 mx-3 d-flex justify-content-center align-items-center">
                                             <img src={Ramanzani} alt="player" height={50} width={50} />
                                         </div>
-                                        <p className='fs-5 bold me-4 text-success'>+5%</p>
+                                        <p className='fs-5 bold me-4 text-success'>{variation}</p> //TODO: Change color depending on the variation
                                     </Container>
                                 </Container>
                                 <Container className='flex-grow-1 p-0 m-0' fluid>
-                                    <ChartComponent data={initialData} />
+                                    <ChartComponent data={data} />
                                 </Container>
                             </Container>
                         </Row>
