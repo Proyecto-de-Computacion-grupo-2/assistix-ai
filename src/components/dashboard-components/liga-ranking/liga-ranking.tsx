769bee8d5200cf657cbbeb6924538f7140640a9f
LCabelloC <lcabelloms@gmail.com>
2024-03-15 14:01:35 +0100

    Modificaciones en el dashboard tabla nueva y cambio con la tabla de la liga actual

--- src/components/dashboard-components/liga-ranking/liga-ranking.tsx
+++ src/components/dashboard-components/liga-ranking/liga-ranking.tsx
@@ -2,21 +2,37 @@ import { Container } from 'react-bootstrap'
 import { LeftArrowIcon, RightArrowIcon } from '../../icons/icons'
 import './liga-ranking.scss'
 
+import laLigaLogo from '../../../assets/laLigaLogo.jpg'
+import misterLogo from '../../../assets/misterLogo.png'
+import { useState } from 'react'
+import LocalLeague from './local-league/local-league'
+
 export default function LigaRanking() {
+
+    const [laLiga, setLaLiga] = useState([laLigaLogo, misterLogo])
+    const [isMister, setIsMister] = useState(false)
+
+    const changeLeague = () => {
+        setLaLiga([laLiga[1], laLiga[0]])
+        setIsMister(!isMister)
+    }
+
     return (
         <Container className="w-100 bg-white rounded-4 h-100 d-flex flex-column p-0 me-1 mb-1" style={{ minHeight: '30vh' }} fluid>
             <Container className="py-1 border-bottom m-0">
                 <strong className="text-secondary">Tabla clasificación</strong>
                 <Container className="d-flex justify-content-around align-items-center">
-                    <p>Mister liga</p>
-                    <button className='hover-change'><LeftArrowIcon fill='white' /> </button>
-                    <strong className='fs-5'>La liga</strong>
-                    <button className='hover-change' > <RightArrowIcon fill='white' /></button>
-                    <p>Mister liga</p>
+                    <img src={laLiga[1]} width={20} height={20} />
+                    <button className='hover-change' onClick={changeLeague}><LeftArrowIcon fill='white' /> </button>
+                    <img src={laLiga[0]} width={20} height={20} />
+                    <button className='hover-change' onClick={changeLeague}> <RightArrowIcon fill='white' /></button>
+                    <img src={laLiga[1]} width={20} height={20} />
                 </Container>
             </Container>
             <Container className="flex-grow-1 px-1" style={{ maxHeight: '28vh' }}>
-                {/* <iframe id="sofa-standings-embed-36-52376" src="https://widgets.sofascore.com/es-ES/embed/tournament/36/season/52376/standings/LaLiga?widgetTitle=LaLiga&showCompetitionLogo=true&v=2" style={{ width: '100%', height: '100%', maxWidth: '100%', maxHeight: '100' }} /> */}
+                {
+                    isMister ? <LocalLeague /> : <iframe id="sofa-standings-embed-36-52376" src="https://widgets.sofascore.com/es-ES/embed/tournament/36/season/52376/standings/LaLiga?widgetTitle=LaLiga&showCompetitionLogo=true&v=2" style={{ width: '100%', height: '95%', maxWidth: '100%', maxHeight: '100' }} />
+                }
             </Container>
         </Container>
     )
---
+++ src/components/dashboard-components/liga-ranking/local-league/local-league.tsx
@@ -0,0 +1,86 @@
+import { Col, Container, Row } from "react-bootstrap";
+import RankingCard from "./local-card/local-card";
+
+const tablaLocal = {
+    'table': [
+        {
+            'position': 1,
+            'name': 'admin',
+            'points': 975,
+            'value': 100
+        },
+        {
+            'position': 2,
+            'name': 'M31-G6-AcMilanesa',
+            'points': 956,
+            'value': 100
+        }, {
+            'position': 3,
+            'name': 'admin',
+            'points': 975,
+            'value': 100
+        },
+        {
+            'position': 4,
+            'name': 'M31-G6-AcMilanesa',
+            'points': 956,
+            'value': 100
+        },
+        {
+            'position': 5,
+            'name': 'admin',
+            'points': 975,
+            'value': 100
+        },
+        {
+            'position': 6,
+            'name': 'M31-G6-AcMilanesa',
+            'points': 956,
+            'value': 100
+        },
+        {
+            'position': 7,
+            'name': 'admin',
+            'points': 975,
+            'value': 100
+        },
+        {
+            'position': 8,
+            'name': 'M31-G6-AcMilanesa',
+            'points': 956,
+            'value': 100
+        }
+
+    ]
+}
+
+export default function LocalLeague() {
+    return (
+        <Container className="h-100 p-0 d-flex align-items-center flex-column " fluid>
+            <Row className="h-auto d-flex justify-content-around border-bottom w-100" fluid>
+                <Col sm={1} className="text-center p-0 m-0">
+                    Pos
+                </Col>
+                <Col sm={5} className="text-center p-0 m-0">
+                    Usuario
+                </Col>
+                <Col sm={3} className="text-center p-0 m-0">
+                    Puntos
+                </Col>
+                <Col sm={3} className="text-center p-0 m-0">
+                    Valor
+                </Col>
+            </Row>
+            <Container className="flex-grow-1 px-1 scroll-section d-flex flex-column align-items-center" style={{ maxHeight: '20vh' }}>
+                {
+                    tablaLocal.table.map((team, index) => {
+                        return (
+                            <RankingCard team={team} key={index} />
+                        )
+                    })
+                }
+            </Container>
+
+        </Container>
+    )
+}
