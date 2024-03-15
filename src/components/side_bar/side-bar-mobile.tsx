06bf1f73102d0d474ad5c800d44cfb602b0d9fcf
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-15 15:57:01 +0100

    Better header in mobil

--- src/components/side_bar/side-bar-mobile.tsx
+++ src/components/side_bar/side-bar-mobile.tsx
@@ -1,11 +1,10 @@
 import {useState} from 'react';
+import SideBarNavMoney from "./side-bar-nav-money.tsx";
 
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import SideBarNavMoney from "./side-bar-nav-money.tsx";
 
 import logo from '../../assets/assistix-ai-logo.png'
-
 import './side_bar.scss'
 
 export default function SideBarMobile() {
@@ -17,7 +16,7 @@ export default function SideBarMobile() {
     return (
         <div className='p-0 m-0'>
             <div
-                className='d-flex flex-row  w-100 align-items-center justify-content-around link-offset-2 link-underline link-underline-opacity-0 fs-5 fw-bold text-black rounded-4  color-white'>
+                className='d-flex flex-row  w-100 align-items-center justify-content-between link-offset-2 link-underline link-underline-opacity-0 fs-5 fw-bold text-black rounded-4 color-white ps-5 pe-5  '>
                 <div className='d-flex align-items-center'>
                     <img src={logo} alt="logo" className="fluid m-0 p-0" style={{width: '60px', height: '60px'}}/>
                     <p>ASSISTIX AI</p>
