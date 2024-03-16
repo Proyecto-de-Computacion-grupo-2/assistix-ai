0d71cbb41fe9a975bff20d997c93f5b374464745
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-16 13:46:47 +0100

    Made changes to the market

--- src/components/dashboard-components/recomendation-section/recomendation-card/change-recomendation-card.tsx
+++ src/components/dashboard-components/recomendation-section/recomendation-card/change-recomendation-card.tsx
@@ -1,22 +1,21 @@
-import Ramanzani from '../../../../assets/40090.png'
+import { Container } from "react-bootstrap";
 import { SubstitutionIcon } from '../../../icons/icons'
+import Ramanzani from '../../../../assets/40090.png'
 
 export default function ChangeRecomendationCard() {
 
-    //Este componente recibirá por parámetros los jugadores que deben de ser cambiados
-
     return (
-        <div className="container border-top border-bottom d-flex flex-row justify-content-around align-items-center py-1">
-            <div className='bg-light d-flex justify-content-center align-items-center rounded-4 border border-dark' style={{ height: '60px', width: '60px' }}>
+        <Container className="container border-top border-bottom d-flex flex-row justify-content-around align-items-center py-1" fluid>
+            <Container className='bg-light d-flex justify-content-center align-items-center rounded-4 border border-dark' style={{ height: '60px', width: '60px' }} fluid>
                 <img src={Ramanzani} alt="" style={{ height: '50px', width: '50px' }} />
-            </div>
-            <div className="d-flex flex-column justify-content-center align-items-center">
+            </Container>
+            <Container className="d-flex flex-column justify-content-center align-items-center" fluid>
                 <strong>Cambia</strong>
                 <SubstitutionIcon fill='black' />
-            </div>
-            <div className='bg-light d-flex justify-content-center align-items-center rounded-4 border border-dark' style={{ height: '60px', width: '60px' }}>
+            </Container>
+            <Container className='bg-light d-flex justify-content-center align-items-center rounded-4 border border-dark' style={{ height: '60px', width: '60px' }} fluid>
                 <img src={Ramanzani} alt="" style={{ height: '50px', width: '50px' }} />
-            </div>
-        </div>
+            </Container>
+        </Container>
     )
 }
--- src/components/dashboard-components/recomendation-section/recomendation.tsx
+++ src/components/dashboard-components/recomendation-section/recomendation.tsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import ChangeRecomendationCard from "./recomendation-card/change-recomendation-card";
 import RecomendtionCard from "./recomendation-card/recomendation-card";
 import './recomendation.scss'
@@ -8,28 +8,26 @@ export default function RecomendationSection() {
     const recomendations = true;
 
     return (
-        <Container className="w-100 bg-white rounded-4 h-100 d-flex flex-column p-0 ms-1 mb-1" style={{ minHeight: '30vh' }} fluid>
-            <Container className="py-1 border-bottom m-0">
+        <Container className="w-100 bg-white rounded-4 h-100 d-flex flex-column p-0 ms-1 mb-1"
+                   style={{minHeight: '30vh'}} fluid>
+            <Container className="py-1 border-bottom m-0" fluid>
                 <strong className="text-secondary">Recomendaciones Assistix AI</strong>
             </Container>
             {
                 recomendations ?
-
-                    <Container className="flex-grow-1 px-1 scroll-section" style={{ maxHeight: '25vh' }}>
-                        <RecomendtionCard />
-                        <ChangeRecomendationCard />
-                        <RecomendtionCard />
-                        <ChangeRecomendationCard />
-                        <RecomendtionCard />
+                    <Container className="flex-grow-1 px-1 scroll-section" style={{maxHeight: '32vh'}} fluid>
+                        <RecomendtionCard/>
+                        <ChangeRecomendationCard/>
+                        <RecomendtionCard/>
+                        <ChangeRecomendationCard/>
+                        <RecomendtionCard/>
                     </Container>
-
                     :
-
-                    <Container className="flex-grow-1 px-1 scroll-section h-100 d-flex flex-column justify-content-center align-items-center">
+                    <Container
+                        className="flex-grow-1 px-1 scroll-section h-100 d-flex flex-column justify-content-center align-items-center">
                         <p className="text-secondary">No hay recomendaciones</p>
                     </Container>
             }
-
         </Container>
     )
 }
