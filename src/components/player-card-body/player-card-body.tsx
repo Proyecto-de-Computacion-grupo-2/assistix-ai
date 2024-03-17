9be1a30e896ae0938fb927805147175f1fa601f1
Geronimo Basso <geronimobasso@MacBook-Pro-de-Geronimo-2.local>
2024-03-17 11:23:33 +0100

    Added search bar

--- src/components/player-card-body/player-card-body.tsx
+++ src/components/player-card-body/player-card-body.tsx
@@ -1,97 +1,101 @@
-import {Card, Row,Col, Container,} from 'react-bootstrap'
-import './player-card-body.scss'
+import  { useState } from 'react'; // Step 1
+import { Card, Row, Col, Container, Form } from 'react-bootstrap';
+import './player-card-body.scss';
 
-function PlayerCardBody({name, position, value}) {
+function PlayerCardBody({ image,name, position, value,points }) {
     return (
-        <Card className='border-black rounded-4' style={{ width: '15rem' }}>
-            <Card.Img variant="top" src="https://assets-fantasy.llt-services.com/players/t186/p1678/256x256/p1678_t186_1_001_000.png" />
+        <Card className='rounded-5' style={{ width: '15rem' }}>
+            <Card.Img className='bg-light rounded-5' variant="top" src={image}/>
             <Card.Body>
                 <Card.Title className='fw-bold'>{name}</Card.Title>
-                <Card.Subtitle>{value} € || {position}</Card.Subtitle>
+                <Card.Subtitle><p className='fw-bold'>{position}</p> {value}€</Card.Subtitle>
+                <Card.Text>{points} puntos</Card.Text>
             </Card.Body>
         </Card>
     );
 }
 
-
 export default function PlayersGrid() {
+    const [searchQuery, setSearchQuery] = useState('');
 
     const players = [
         {
             name:'Jude Bellingham',
             position: 'MC',
-            value: '10.000.00'
+            value: 1000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t186/p1678/256x256/p1678_t186_1_001_000.png'
+        },
+        {
+            name:'Sergio Herrera',
+            position: 'PT',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t450/p644/256x256/p644_t450_1_001_000.png'
+        },
+        {
+            name:'Florian Lejeune',
+            position: 'DF',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t184/p1208/256x256/p1208_t184_1_001_000.png'
+        },
+        {
+            name:'Jose Copete',
+            position: 'DF',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t181/p1482/256x256/p1482_t181_1_001_000.png'
         },
         {
             name:'Jude Bellingham',
             position: 'MC',
-            value: '10.000.00'
+            value: 1000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t186/p1678/256x256/p1678_t186_1_001_000.png'
         },
         {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
+            name:'Sergio Herrera',
+            position: 'PT',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t450/p644/256x256/p644_t450_1_001_000.png'
         },
         {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
+            name:'Florian Lejeune',
+            position: 'DF',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t184/p1208/256x256/p1208_t184_1_001_000.png'
         },
         {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
+            name:'Jose Copete',
+            position: 'DF',
+            value: 5000000,
+            points: 200,
+            image: 'https://assets-fantasy.llt-services.com/players/t181/p1482/256x256/p1482_t181_1_001_000.png'
         },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        },
-        {
-            name:'Jude Bellingham',
-            position: 'MC',
-            value: '10.000.00'
-        }
     ]
+
+    const filteredPlayers = players.filter(player =>
+        player.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     return (
         <Container className='bg-white rounded-4 scrollable-container' fluid>
             <Row className='gap-4 ms-4 me-4'>
-                <Container fluid className='mt-5 mb-5'>
-                    SEARCH BAR
+                <Container fluid className='mt-5 mb-2'>
+                    <Form.Control
+                        className='rounded-3 bg-light'
+                        type="text"
+                        placeholder="Busacr a un jugador..."
+                        value={searchQuery}
+                        onChange={(e) => setSearchQuery(e.target.value)}
+                    />
                 </Container>
-                {players.map((player, index) => (
-                    <Col key={index} className='gap-2'> {/* Provide a unique key */}
-                        <PlayerCardBody {...player}/> {/* Spread operator to pass props */}
+                {filteredPlayers.map((player, index) => (
+                    <Col key={index} className='gap-2'>
+                        <PlayerCardBody {...player}/>
                     </Col>
                 ))}
             </Row>
