4b8ad703c7bf8bf524224f199e6bfd3fe30d111d
Geronimo <gerobasso01@gmail.com>
2024-04-11 20:03:18 +0200

    Switch botton positions

--- src/components/shared-components/side_bar/side-bar-mobile.tsx
+++ src/components/shared-components/side_bar/side-bar-mobile.tsx
@@ -17,14 +17,14 @@ export default function SideBarMobile() {
         <div className='p-0 m-0'>
             <div
                 className='d-flex flex-row  w-100 align-items-center justify-content-between link-offset-2 link-underline link-underline-opacity-0 fs-5 fw-bold text-black rounded-4 color-white ps-5 pe-5  '>
-                <div className='d-flex align-items-center'>
-                    <img src={logo} alt="logo" className="fluid m-0 p-0" style={{width: '60px', height: '60px'}}/>
-                    <p>ASSISTIX AI</p>
-                </div>
                 <div>
                     <Button variant="dark" onClick={handleShow}>
                         Menu
                     </Button>
+                </div>
+                <div className='d-flex align-items-center'>
+                    <img src={logo} alt="logo" className="fluid m-0 p-0" style={{width: '60px', height: '60px'}}/>
+                    <p>ASSISTIX AI</p>
                 </div>
             </div>
             <Offcanvas show={show} onHide={handleClose}>
