d61c17400c4708d1c6d0f4977a407b27866aa689
Geronimo <gerobasso01@gmail.com>
2024-04-27 19:13:40 +0200

    Added hamburger menu and icons in absence section

--- src/components/player-id-components/absence/absence-section.tsx
+++ src/components/player-id-components/absence/absence-section.tsx
@@ -9,6 +9,23 @@ interface AbsencesSectionProps {
     absences: Absence[];
 }
 
+function mapIconToAbsenceType(absenceType: string) {
+    switch (absenceType) {
+        case "red":
+            return "https://cdn-icons-png.flaticon.com/512/867/867340.png";
+        case "doubt":
+            return "https://cdn-icons-png.flaticon.com/512/14264/14264017.png";
+        case "five":
+            return "https://cdn-icons-png.flaticon.com/512/8676/8676804.png";
+        case "injury":
+            return "https://cdn-icons-png.flaticon.com/512/469/469413.png";
+        case "double":
+            return "https://cdn-icons-png.flaticon.com/512/1845/1845350.png";
+        default:
+            return "https://cdn-icons-png.flaticon.com/512/1828/1828666.png";
+    }
+}
+
 function formatDate(dateString: string) {
     const date = new Date(dateString);
     const year = date.getFullYear();
@@ -22,8 +39,9 @@ function PlayerInjuryCard({absence}: AbsenceSectionProps) {
     return (
         <Row className="m-0 p-0 h-auto border-top border-bottom w-100 align-items-center">
             <Col xs={4} sm={4} className="d-flex flex-column justify-content-center gap-1 p-1 align-items-center">
-                <img className="mx-auto d-block" width={20}></img>
-                <p className="small text-center">{absence.description_absence}</p>
+                <img className="mx-auto d-block" src={mapIconToAbsenceType(absence.type_absence)}
+                     alt={absence.type_absence} width={20}/>
+                <p className="small text-justify">{absence.description_absence}</p>
             </Col>
             <Col xs={4} sm={4} className='align-items-center'>
                 <p className="small text-center">{formatDate(absence.since)}</p>
