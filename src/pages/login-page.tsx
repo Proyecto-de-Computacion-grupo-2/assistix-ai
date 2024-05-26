a5eee10504cc5290d2b737ab5e8f037edf0389a2
Geronimo <gerobasso01@gmail.com>
2024-05-26 18:59:27 +0200

    Added code to parse the JWT

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -4,6 +4,14 @@ import {getAuthToken} from "../services/auth-service/auth-service.ts";
 import assistixLogo from '../assets/images/assistix-ai-logo.png'
 import '../styles/login-page.scss'
 
+function parseJwt(token: string) {
+    const base64Url = token.split('.')[1];
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+}
 
 export default function LoginPage() {
 
@@ -11,12 +19,20 @@ export default function LoginPage() {
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
 
-
     const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const authResponse = await getAuthToken(email, password);
         if (authResponse) {
-            localStorage.setItem('jwtToken', authResponse.access_token);
+            const token = authResponse.access_token;
+
+            // Decode the token and extract the role
+            const decodedToken = parseJwt(token);
+            const role = decodedToken.admin ? 'admin' : 'user';
+
+            // Store the token and role in local storage
+            localStorage.setItem('jwtToken', token);
+            localStorage.setItem('role', role);
+
             navigate('/');
         } else {
             alert('Error. Usuario o contraseña incorrectos');
