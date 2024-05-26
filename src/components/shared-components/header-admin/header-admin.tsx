f0c49a21ca51d76839500be5ba0f0d4dfe1ae4e9
Geronimo <gerobasso01@gmail.com>
2024-05-26 20:17:57 +0200

    Small detail

--- src/components/shared-components/header-admin/header-admin.tsx
+++ src/components/shared-components/header-admin/header-admin.tsx
@@ -14,10 +14,10 @@ export default function HeaderAdmin() {
 
     return (
         <Navbar className='bg-white mt-2 rounded-4 w-100'>
-            <Container  fluid>
+            <Container fluid>
                 <Row className='w-100'>
                     <Col className='d-flex w-100 justify-content-around align-items-center'>
-                        <Container  className='d-flex align-items-center'>
+                        <Container className='d-flex align-items-center'>
                             <img src={logo} alt='Assistix AI Logo' style={{width: 45, height: 45}}/>
                             <h1 className='ms-3'>Assistix AI</h1>
                         </Container>
