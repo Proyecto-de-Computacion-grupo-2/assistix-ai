e7690b395f28221603a33cf4133be4261d667435
LCabelloC <lcabelloms@gmail.com>
2024-05-27 19:56:10 +0200

    Arreglo responsive del pop-up info

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -1,6 +1,6 @@
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import {getAuthToken} from "../services/auth-service/auth-service.ts";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAuthToken } from "../services/auth-service/auth-service.ts";
 import assistixLogo from '../assets/images/assistix-ai-logo.png'
 import '../styles/login-page.scss'
 import { Container, Row } from "react-bootstrap";
@@ -9,7 +9,7 @@ import { InformationIcon } from "../components/shared-components/icons/icons.tsx
 function parseJwt(token: string) {
     const base64Url = token.split('.')[1];
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
     return JSON.parse(jsonPayload);
@@ -107,7 +107,6 @@ export default function LoginPage() {
                         </form>
                         <p className="personalized-text user-select-none" onClick={handleFormStatus}>{isLogin ? '¿No tienes cuenta?' : '¿Ya tienes cuenta?'} <span>{isLogin ? 'Registrate aquí' : 'Inicia sesión aquí'}</span></p>
                     </Container>
-
                 </Container>
             </Container>
         </Row>
@@ -119,7 +118,7 @@ const PopUp = ({ message, description, nameButton, action }: { message: String, 
     return (
 
         <Row className="m-0 p-0 w-100 h-100 personalized-pop-up d-flex justify-content-center align-items-center">
-            <Container className={`m-0 py-2 px-4 ${description === '' ? 'w-50' : 'personalized-width'} bg-light rounded-4 d-flex flex-column align-items-center`}>
+            <Container className={`m-0 py-2 px-4 ${description === '' ? 'personalized-pop-up' : 'personalized-width'} bg-light rounded-4 d-flex flex-column align-items-center`}>
                 <img src={assistixLogo} alt="Assistix Logo" className="m-0 p-0 w-25" />
                 <h2 className="fs-4 text-center user-select-none py-1">{message}</h2>
                 {description === '' ? (
